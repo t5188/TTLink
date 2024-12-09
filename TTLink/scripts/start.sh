ce606fe2823393f87ca4c4cf8fe156bb2aacc29b
Tony <84645195+t5188@users.noreply.github.com>
2024-12-09 22:59:05 +0800

    Update start.sh

--- TTLink/scripts/start.sh
+++ TTLink/scripts/start.sh
@@ -15,30 +15,30 @@ cd ${scripts_dir}
 export PATH="/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH"
 
 proxy_service() {
-if [ ! -f "${module_dir}/disable" ]; then
-  log Info "Module Enabled"
-  log Info "start TTLink"
-    if [ "${network_mode}" = "tproxy" ]; then
-        ${scripts_dir}/TTLink_tproxy.service enable > /dev/null 2>&1 && \
-        ${scripts_dir}/TTLink_tproxy.service description > /dev/null 2>&1
+    if [ ! -f "${module_dir}/disable" ]; then
+        log Info "Module Enabled"
+        log Info "Start TTLink"
+        if [ "${network_mode}" = "tproxy" ]; then
+            ${scripts_dir}/TTLink_tproxy.service enable > /dev/null 2>&1 && \
+            ${scripts_dir}/TTLink_tproxy.service description > /dev/null 2>&1
+        else
+            ${scripts_dir}/TTLink_tun.service enable > /dev/null 2>&1 && \
+            ${scripts_dir}/TTLink_tun.service description > /dev/null 2>&1
+        fi
     else
-        ${scripts_dir}/TTLink_tun.service enable > /dev/null 2>&1 && \
-        ${scripts_dir}/TTLink_tun.service description > /dev/null 2>&1
+        log Warn "Module Disabled"
+        log Info "Module Disabled" > ${scripts_dir}/run.log
     fi
-else
-  log Warn "Module Disabled"
-  log Info "Module Disabled" > ${scripts_dir}/run.log
-fi
 }
 
 start_inotifyd() {
-  PIDs=($(busybox pidof inotifyd))
-  for PID in "${PIDs[@]}"; do
-    if grep -q "${scripts_dir}/TTLink.inotify" "/proc/$PID/cmdline"; then
-      return
-    fi
-  done
-  inotifyd "${scripts_dir}/TTLink.inotify" "${module_dir}" > /dev/null 2>&1 &
+    PIDs=($(busybox pidof inotifyd))
+    for PID in "${PIDs[@]}"; do
+        if grep -q "${scripts_dir}/TTLink.inotify" "/proc/$PID/cmdline"; then
+            return
+        fi
+    done
+    inotifyd "${scripts_dir}/TTLink.inotify" "${module_dir}" > /dev/null 2>&1 &
 }
 
 proxy_service
