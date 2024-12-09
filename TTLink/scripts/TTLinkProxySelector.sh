27d6e3aa56d5b0cb9adc9ee83093c798883db62b
Tony <84645195+t5188@users.noreply.github.com>
2024-12-09 17:33:36 +0800

    更新 TTLinkProxySelector.sh

--- TTLink/scripts/TTLinkProxySelector.sh
+++ TTLink/scripts/TTLinkProxySelector.sh
@@ -7,7 +7,7 @@ scripts_dir=$(dirname ${scripts})
 parent_dir=$(dirname ${scripts_dir})
 module_dir="/data/adb/modules/TTLink"
 settings_file=${scripts_dir}/settings.ini
-json_dir="${parent_dir}/confs"  # 假设 JSON 文件存放在此目录下
+sing_config=${parent_dir}/confs/box_config.json
 yq=${parent_dir}/binary/yq
 
 # 颜色定义
@@ -27,9 +27,9 @@ if [ ! -f "$settings_file" ]; then
     exit 1
 fi
 
-# 检查是否存在任何 JSON 文件
-if [ ! -n "$(find $json_dir -name "*.json" -print -quit)" ]; then
-    echo -e "${red}未找到任何 JSON 文件，无法继续${normal}"
+# 检查 box_config.json 文件是否存在
+if [ ! -f "$sing_config" ]; then
+    echo -e "${red}box_config.json 文件不存在，无法继续${normal}"
     exit 1
 fi
 
@@ -80,78 +80,62 @@ select mode in "tproxy" "tun" "退出"; do
             echo -e "${green}您选择的代理方式是: tproxy${normal}"
             # 更新 settings.ini 中的代理模式
             sed -i "s/^network_mode=.*/network_mode=tproxy/" $settings_file
-            # 遍历文件夹中的 JSON 文件，直到找到第一个包含 inbounds 字段的文件
-            for json_file in "$json_dir"/*.json; do
-                # 检查文件中是否包含 inbounds 字段
-                if $yq 'has("inbounds")' "$json_file"; then
-                    echo "正在修改文件: $json_file"
-                    $yq '.inbounds = [] | .inbounds += [{
-                        "type": "tproxy",
-                        "tag": "tproxy-in",
-                        "listen": "::",
-                        "listen_port": 1536,
-                        "sniff": true,
-                        "sniff_override_destination": true
-                    }]' -i --output-format=json "$json_file"
-                    break  # 找到第一个符合条件的文件后退出循环
-                fi
-            done
+            # 使用 yq 修改 sing_config 配置
+            $yq '.inbounds = [] | .inbounds += [{
+                "type": "tproxy",
+                "tag": "tproxy-in",
+                "listen": "::",
+                "listen_port": 1536,
+                "sniff": true,
+                "sniff_override_destination": true
+            }]' -i --output-format=json "${sing_config}"
             break
             ;;
-
         "tun")
             echo -e "${green}您选择的代理方式是: tun${normal}"
             # 更新 settings.ini 中的代理模式
             sed -i "s/^network_mode=.*/network_mode=tun/" $settings_file
-            # 遍历文件夹中的 JSON 文件，直到找到第一个包含 inbounds 字段的文件
-            for json_file in "$json_dir"/*.json; do
-                # 检查文件中是否包含 inbounds 字段
-                if $yq 'has("inbounds")' "$json_file"; then
-                    echo "正在修改文件: $json_file"
-                    $yq '.inbounds = [] | .inbounds += [{
-                        "type": "tun",
-                        "tag": "tun-in",
-                        "interface_name": "tun0",
-                        "mtu": 1400,
-                        "auto_route": true,
-                        "strict_route": true,
-                        "endpoint_independent_nat": true,
-                        "address": [
-                            "172.18.0.1/30",
-                            "fdfe:dcba:9876::1/126"
-                        ],
-                        "route_exclude_address": [
-                            "127.0.0.0/16",
-                            "fc00::/7",
-                            "168.138.170.58/32",
-                            "138.2.83.107/32",
-                            "146.235.16.222/32",
-                            "213.35.97.236/32",
-                            "129.150.50.129/32",
-                            "146.56.128.39/32",
-                            "152.70.244.79/32"
-                        ],
-                        "stack": "system",
-                        "sniff": true,
-                        "sniff_override_destination": true
-                    }]' -i --output-format=json "$json_file"
-                    break  # 找到第一个符合条件的文件后退出循环
-                fi
-            done
+            # 使用 yq 修改 sing_config 配置
+            $yq '.inbounds = [] | .inbounds += [{
+                "type": "tun",
+                "tag": "tun-in",
+                "interface_name": "tun0",
+                "mtu": 1400,
+                "auto_route": true,
+                "strict_route": true,
+                "endpoint_independent_nat": true,
+                "address": [
+                    "172.18.0.1/30",
+                    "fdfe:dcba:9876::1/126"
+                ],
+                "route_exclude_address": [
+                    "127.0.0.0/16",
+                    "fc00::/7",
+                    "168.138.170.58/32",
+                    "138.2.83.107/32",
+                    "146.235.16.222/32",
+                    "213.35.97.236/32",
+                    "129.150.50.129/32",
+                    "146.56.128.39/32",
+                    "152.70.244.79/32"
+                ],
+                "stack": "system",
+                "sniff": true,
+                "sniff_override_destination": true
+            }]' -i --output-format=json "${sing_config}"
             break
             ;;
-
         "退出")
             echo -e "${green}退出程序${normal}"
             break
             ;;
-
         *)
             echo -e "${red}输入无效，请重新选择${normal}"
             ;;
-
     esac
 done
 
 # 确保代理软件再次启用
 rm -f "${module_dir}/disable"
+
+# TTLinkProxySelector.sh
