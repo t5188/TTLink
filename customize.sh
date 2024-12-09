ee22421a48bceacc7d2f4f47a9c4052ff5fcfbe4
Tony <84645195+t5188@users.noreply.github.com>
2024-12-09 16:41:22 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -40,7 +40,7 @@ unzip -j -o "$ZIPFILE" "module.prop" -d $MODPATH >&2
 unzip -j -o "$ZIPFILE" "system.prop" -d $MODPATH >&2
 ui_print "- 正在设置权限"
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive /data/adb/TTLink/ 0 3005 0755 0644
+set_perm_recursive /data/adb/TTLink/ 0 3005 0755 0755
 set_perm_recursive /data/adb/TTLink/scripts/ 0 3005 0755 0700
 set_perm /data/adb/service.d/TTLink.sh 0 0 0755
 set_perm $MODPATH/uninstall.sh 0 0 0755
