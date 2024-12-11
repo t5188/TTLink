330e962a590295bd33e9d6c1e7015021824360ac
coomsn <coomsn@qq.com>
2024-12-11 10:47:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -70,8 +70,8 @@ if [ -n "$largest_folder" ]; then
       if [ -d "$folder/confs" ]; then
         cp -rf "$folder/confs/"* /data/adb/TTLink/confs/
         ui_print "- Copied contents of $folder/confs to /data/adb/TTLink/confs/"
-        cp -rf "$folder/xray/"* /data/adb/TTLink/xray/
-        ui_print "- Copied contents of $folder/xray to /data/adb/TTLink/xray/"
+        cp -rf "$folder/confx/"* /data/adb/TTLink/confx/
+        ui_print "- Copied contents of $folder/confx to /data/adb/TTLink/confx/"
         ui_print "- 成功还原配置文件"
       fi
       break
