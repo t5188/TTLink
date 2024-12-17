b30b7c7704a90015575335a6d31f4400e0ce8e4b
coomsn <coomsn@qq.com>
2024-12-17 23:26:09 +0800

    Update start.sh

--- TTLink/scripts/start.sh
+++ TTLink/scripts/start.sh
@@ -11,43 +11,52 @@ source "${scripts_dir}/TTLink.service"
 cd ${scripts_dir}
 
 proxy_service() {
-    if [ ! -f "${module_dir}/disable" ]; then
-        log Info "Module Enabled"
-        log Info "Start TTLink"
-        ${scripts_dir}/TTLink.service enable >/dev/null 2>&1 && \
-        ${scripts_dir}/TTLink.service description >/dev/null 2>&1
-    else
-        log Warn "Module Disabled"
-        log Info "Module Disabled" >${parent_dir}/log/run.log
-    fi
+  if [ ! -f "${module_dir}/disable" ]; then
+    log Info "Module Enabled"
+    log Info "Start TTLink"
+    ${scripts_dir}/TTLink.service enable >/dev/null 2>&1 &&
+      ${scripts_dir}/TTLink.service description >/dev/null 2>&1
+  else
+    log Warn "Module Disabled"
+    log Info "Module Disabled" >${parent_dir}/log/run.log
+  fi
 }
 
 net_inotifyd() {
-  while [ ! -f /data/misc/net/rt_tables ] ; do
+  while [ ! -f /data/misc/net/rt_tables ]; do
     sleep 3
   done
 
   net_dir="/data/misc/net"
-  
+
   for PID in "${PIDs[@]}"; do
-      if grep -q "${scripts_dir}/net.inotify" "/proc/$PID/cmdline"; then
-          return
-      fi
+    if grep -q "${scripts_dir}/net.inotify" "/proc/$PID/cmdline"; then
+      return
+    fi
   done
   inotifyd "${scripts_dir}/net.inotify" "${net_dir}" >/dev/null 2>&1 &
 }
 
 start_inotifyd() {
-    PIDs=($(busybox pidof inotifyd)) # Environment variables are required.
-    net_inotifyd
-    for PID in "${PIDs[@]}"; do
-        if grep -q "${scripts_dir}/TTLink.inotify" "/proc/$PID/cmdline"; then
-            return
-        fi
-    done
-    inotifyd "${scripts_dir}/TTLink.inotify" "${module_dir}" >/dev/null 2>&1 &
+  PIDs=($(busybox pidof inotifyd)) # Environment variables are required.
+  net_inotifyd
+  for PID in "${PIDs[@]}"; do
+    if grep -q "${scripts_dir}/TTLink.inotify" "/proc/$PID/cmdline"; then
+      return
+    fi
+  done
+  inotifyd "${scripts_dir}/TTLink.inotify" "${module_dir}" >/dev/null 2>&1 &
 }
 
+check_network() {
+  TARGET="1.1.1.1"
+  while ! ping -c 1 -W 1 ${TARGET} >/dev/null 2>&1; do
+    sleep 3
+  done
+  log Info "网络已连接！"
+}
+
+check_network
 proxy_service
 start_inotifyd
 
