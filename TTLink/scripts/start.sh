b2c9fe0d84e1c0e1e872acfae570a2742ca7caf8
coomsn <coomsn@qq.com>
2024-12-20 09:50:38 +0800

    fixed
    Remove unnecessary code

--- TTLink/scripts/start.sh
+++ TTLink/scripts/start.sh
@@ -19,8 +19,7 @@ proxy_service() {
   if [[ ! -f "${module_dir}/disable" ]]; then
     log Info "Module Enabled"
     log Info "Start TTLink"
-    ${scripts_dir}/TTLink.service enable >/dev/null 2>&1 &&
-      ${scripts_dir}/TTLink.service description >/dev/null 2>&1
+    ${scripts_dir}/TTLink.service enable >/dev/null 2>&1
   else
     log Warn "Module Disabled"
     log Info "Module Disabled" >${parent_dir}/log/run.log
