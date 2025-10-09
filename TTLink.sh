532e9b849a5596d29e47e3cf2df8e7eb2f821ecf
coomsn <coomsn@qq.com>
2025-10-09 19:52:20 +0800

    Update TTLink.sh

--- TTLink.sh
+++ TTLink.sh
@@ -1,14 +1,3 @@
 #!/system/bin/sh
-
 scripts_dir="/data/adb/TTLink/scripts"
-(
-until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
-    sleep 3
-done
-
-chmod 755 "${scripts_dir}/start.sh"
-
-"${scripts_dir}/start.sh"
-) &
-
-# TTLink.sh
+(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 3; done; "${scripts_dir}/start.sh") &
