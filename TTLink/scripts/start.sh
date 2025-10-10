59c04c7f3a605d8c609dc8254b66cce9f92325df
coomsn <coomsn@qq.com>
2025-10-10 17:57:18 +0800

    update

--- TTLink/scripts/start.sh
+++ TTLink/scripts/start.sh
@@ -1,18 +1,14 @@
 #!/system/bin/sh
-clear
-scripts=$(realpath $0)
-scripts_dir=$(dirname ${scripts})
-parent_dir=$(dirname ${scripts_dir})
-
+cd ${0%/*} # current working directory
 # source files
-source "${scripts_dir}/settings.ini"
-source "${scripts_dir}/TTLink.service"
+source "$(pwd)/settings.ini"
+source "$(pwd)/TTLink.service"
 
 proxy_service() {
   if [[ ! -f "${module_dir}/disable" ]]; then
     log Info "Module Enabled"
     log Info "Start TTLink"
-    ${scripts_dir}/TTLink.service enable >/dev/null 2>&1
+    $(pwd)/TTLink.service enable >/dev/null 2>&1
   else
     log Warn "Module Disabled"
     log Info "Module Disabled" >${parent_dir}/log/run.log
@@ -27,22 +23,22 @@ net_inotifyd() {
   net_dir="/data/misc/net"
 
   for PID in "${PIDs[@]}"; do
-    if grep -q "${scripts_dir}/net.inotify" "/proc/$PID/cmdline"; then
+    if grep -q "$(pwd)/net.inotify" "/proc/$PID/cmdline"; then
       return
     fi
   done
-  inotifyd "${scripts_dir}/net.inotify" "${net_dir}" >/dev/null 2>&1 &
+  inotifyd "$(pwd)/net.inotify" "${net_dir}" >/dev/null 2>&1 &
 }
 
 start_inotifyd() {
   PIDs=($(busybox pidof inotifyd)) # Environment variables are required.
   net_inotifyd
   for PID in "${PIDs[@]}"; do
-    if grep -q "${scripts_dir}/TTLink.inotify" "/proc/$PID/cmdline"; then
+    if grep -q "$(pwd)/TTLink.inotify" "/proc/$PID/cmdline"; then
       return
     fi
   done
-  inotifyd "${scripts_dir}/TTLink.inotify" "${module_dir}" >/dev/null 2>&1 &
+  inotifyd "$(pwd)/TTLink.inotify" "${module_dir}" >/dev/null 2>&1 &
 }
 
 proxy_service
